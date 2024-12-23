9c87984a33b0657ad4099190af3bbccd88a6498f
Edlly <edlly@edlly.de>
2024-12-23 11:55:30 +0100

    WordCount in memory store

--- backend/services/src/main/java/de/edlly/key/services/WordCountMapperStore.java
+++ backend/services/src/main/java/de/edlly/key/services/WordCountMapperStore.java
@@ -3,10 +3,13 @@ package de.edlly.key.services;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import de.edlly.key.entities.store.WordMap;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Component
 public class WordCountMapperStore {
 
     // Cache to prevent OOM failure
@@ -16,8 +19,8 @@ public class WordCountMapperStore {
         cache.put(wordMap.getPost().getId(), wordMap);
     }
 
-    public WordMap getWordMap(Long postId) {
-        return cache.asMap().get(postId);
+    public Optional<WordMap> getWordMap(Long postId) {
+        return Optional.ofNullable(cache.asMap().get(postId));
     }
 
     public List<Long> getAllPostIdsInCache(Long postId) {
---
+++ backend/services/src/test/java/de/edlly/key/services/WordCountMapperStoreTest.java
@@ -0,0 +1,51 @@
+package de.edlly.key.services;
+
+import de.edlly.key.entities.store.WordMap;
+import de.edlly.key.entities.wp.Post;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = WordCountMapperStore.class)
+class WordCountMapperStoreTest {
+
+    @Autowired
+    WordCountMapperStore store;
+
+    @Test
+    public void testAddPostAndGet() {
+        WordMap wordMap = createDummyWordMapEntity();
+
+        store.addWordMap(wordMap);
+
+        Optional<WordMap> actual = store.getWordMap(1L);
+        assertNotNull(actual);
+        assertTrue(actual.isPresent());
+        assertEquals(1L, actual.get().getPost().getId());
+    }
+
+    @Test
+    public void testAddPostAndNotFound() {
+        WordMap wordMap = createDummyWordMapEntity();
+
+        store.addWordMap(wordMap);
+
+        Optional<WordMap> actual = store.getWordMap(2L);
+        assertNotNull(actual);
+        assertFalse(actual.isPresent());
+    }
+
+    private WordMap createDummyWordMapEntity() {
+        Post post = new Post();
+        post.setId(1L);
+        WordMap wordMap = new WordMap();
+        wordMap.setPost(post);
+        wordMap.getWords().put(1, "and");
+        return wordMap;
+    }
+
+}
