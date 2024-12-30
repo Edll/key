9b5fcf1e0c13e60ea44c45fc9e6dac009fa22376
Edlly <edlly@edlly.de>
2024-12-30 12:09:13 +0100

    autoconnection websocket

--- frontend/src/hooks/useWebsocket.ts
+++ frontend/src/hooks/useWebsocket.ts
@@ -1,14 +1,15 @@
 import {ActivationState, Client} from '@stomp/stompjs';
 import {IWordMapData} from "../interfaces/IWordMapData.ts";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 import {WS_ENDPOINT} from "../index.tsx";
 
 export const useWebsocket = () => {
 
     const [data, setData] = useState<IWordMapData | null>(null)
-
     const [client, setClient] = useState<Client | null>(null)
     const [error, setError] = useState<string | null>(null)
+
+    const [connectOnStart, setConnectOnStart] = useState(false)
 
     useEffect(() => {
         const _client = new Client({
@@ -49,13 +50,26 @@ export const useWebsocket = () => {
         setClient(_client);
     }, []);
 
+    const isActive = useMemo(() => {
+        let state = client?.state === ActivationState.ACTIVE;
+        console.log(state ? "Websocket is active" : "Websocket is inactive");
+        return state;
+    }, [client?.state]);
+
     const activateClient = useCallback(() => {
-        console.log(`Try activating client`);
+        console.log(`Try activating client`, client !== undefined);
         client?.activate();
         setError(null)
     }, [client]);
 
-    const online = client?.state === ActivationState.ACTIVE;
+    useEffect(() => {
+        if(!connectOnStart && !isActive && client){
+            console.log("Starting client", client)
+            activateClient();
+            setConnectOnStart(true);
+        }
+    }, [connectOnStart, setConnectOnStart, isActive, activateClient, client]);
 
-    return {activateClient: activateClient, data: data, online: online, error: error}
+
+    return {activateClient: activateClient, data: data, online: isActive, error: error}
 }
